1439864b941321446ad63590c92bc1941192e438
PuaR0 <92799139+PuaR0@users.noreply.github.com>
2022-06-19 12:55:52 +0300

    Initial Commit

---
+++ LoginApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp2
+//
+//  Created by Алиага С on 19.6.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
